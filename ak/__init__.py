ea90d779f0c94760d2605c0869c9abc7e214f8a7
Sébastien BEAU <sebastien.beau@akretion.com>
2016-06-13 11:59:44 +0200

    [IMP] add setup file for installation

---
+++ ak/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# coding: utf-8
+from .main import Ak
+
+__version__ = '1.0.0'
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import re
+
+def read(*parts):
+    path = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(path, encoding='utf-8') as fobj:
+        return fobj.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+setup(
+    name='ak',
+    version=find_version("ak", "__init__.py"),
+    author='Akretion',
+    author_email='contact@akretion.com',
+    url='https://github.com/akretion/voodoo-cli/',
+    description='simple cli for Odoo',
+    license="AGPLv3+",
+    long_description=open('README.md').read(),
+    install_requires=[
+        r.strip() for r in open('requirement.txt').read().splitlines() ],
+    entry_points="""
+    [console_scripts]
+    ak=ak.main:main
+    """,
+    include_package_data=True,
+    packages = find_packages() + ['ak'],
+    zip_safe=False,
+)
