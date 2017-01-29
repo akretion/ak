35d06c0fe6c901b47d991274dab4fda8bb1dcfa8
Sébastien BEAU <sebastien.beau@akretion.com>
2017-01-29 17:56:57 +0100

    [BUMP] bump version 1.3.0

--- ak/main.py
+++ ak/main.py
@@ -15,7 +15,7 @@ import ConfigParser
 from plumbum.commands.base import BaseCommand
 
 
-__version__ = '1.2.5'
+__version__ = '1.3.0'
 
 BUILDOUT_URL = ('https://raw.githubusercontent.com/buildout/'
                 'buildout/master/bootstrap/bootstrap.py')
