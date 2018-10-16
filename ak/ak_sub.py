f70a539ab5547f33272450bacd7e8a4e75aadc77
hparfr <raph@hpar.fr>
2018-10-16 17:46:04 +0200

    Increment version number

--- ak/ak_sub.py
+++ ak/ak_sub.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     import ConfigParser as configparser
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 ERP_CFG = local.env.get('ERP_CFG_PATH', 'odoo.cfg')
 WORKSPACE = '.'
