fc7c58ce66b3db18efff7c3aec158d9d36e235f9
Hpar <raphael.reverdy@akretion.com>
2021-04-27 08:27:27 +0000

    Remove old files

--- ak/__init__.py
+++ ak/__init__.py
@@ -2,11 +2,6 @@
 # coding: utf-8
 from .main import Ak
 from . import ak_sub
-# from . import ak_console
-# from . import ak_init
-from . import ak_db
-from . import ak_other
 from . import ak_build
-from . import ak_migrate
 from . import ak_suggest
 from . import ak_sparse
