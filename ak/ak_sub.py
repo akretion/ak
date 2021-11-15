1eff02bc298b9ee777369185abf8b6f1183222e9
hparfr <raphael.reverdy@akretion.com>
2021-11-15 15:24:25 +0100

    Fix clone with odoo dir already existing

--- ak/ak_sub.py
+++ ak/ak_sub.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     import ConfigParser as configparser
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 
 ERP_CFG = local.env.get('ERP_CFG_PATH', 'odoo.cfg')
 WORKSPACE = '.'
